526f8e0d7c7ddf02632adcc09152cf5f3bc34949
Rodrigo Peplau <rodrigo.peplau1@accesscfa.com>
2023-03-04 15:42:17 -0300

    Stable in Helix format

---
+++ src/Feature/Feature.Personalize/Feature.Personalize/App_Start/RouteConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Feature.Personalize.App_Start
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapRoute(
+                name: "personalizeConnect",
+                url: "PersonalizeConnect/{action}",
+                defaults: new { controller = "PersonalizeConnect", action = "Index" }
+            );
+        }
+    }
+}
---
+++ src/Feature/Feature.Personalize/Feature.Personalize/Rules/PersonalizeConnectCondition.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Rules;
+using Sitecore.Rules.Conditions;
+
+namespace Feature.Personalize.Rules
+{
+    public class PersonalizeConnectCondition<T> : StringOperatorCondition<T> where T : RuleContext
+    {
+        public string experienceId { get; set; }
+        public string comparesTo { get; set; }
+        public string experienceValue { get; set; }
+
+        protected override bool Execute(T ruleContext)
+        {
+            Assert.ArgumentNotNull(ruleContext, "ruleContext");
+            OperatorId = comparesTo;
+            string currentExperienceId = ruleContext.Parameters["experienceId"]?.ToString();
+            string currentExperienceValue = ruleContext.Parameters["experienceValue"]?.ToString();
+            return Compare(currentExperienceId, experienceId) && Compare(currentExperienceValue, experienceValue);
+        }
+    }
+}
